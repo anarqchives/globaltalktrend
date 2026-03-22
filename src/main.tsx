aadfd839ff8e3f7369ddbbff9458f637d5e7bc51
ana <anapaulatbarth@gmail.com>
2026-03-22 19:23:01 -0300

    feat: add Vercel Analytics and Speed Insights
    Signed-off-by: ana <anapaulatbarth@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+inject();
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
