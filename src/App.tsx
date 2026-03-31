57335342a5523de701948b84d9bd7e855c41c533
Ana Paula <anapaulatbarth@gmail.com>
2026-03-31 20:46:03 -0300

    arch: QueryClient config, ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import Maintenance from "./pages/Maintenance";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 /*
  * ██  MAINTENANCE MODE  ██
@@ -13,7 +23,9 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <LanguageProvider>
-      <Maintenance />
+      <ErrorBoundary>
+        <Maintenance />
+      </ErrorBoundary>
     </LanguageProvider>
   </QueryClientProvider>
 );
